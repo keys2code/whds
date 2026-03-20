401d10d6bc76126105b204a57200cadaa6b39e07
Phil <phil@phillipkeys.com>
2026-03-20 14:22:22 +0700

    Export new components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,3 @@
-export { Button, buttonVariants } from "./components/button.js"
-export { Switch } from "./components/switch.js"
 export {
   Alert,
   AlertAction,
@@ -14,7 +12,80 @@ export {
   AvatarGroupCount,
   AvatarBadge,
 } from "./components/avatar.js"
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  BreadcrumbEllipsis,
+} from "./components/breadcrumb.js"
+export { Button, buttonVariants } from "./components/button.js"
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from "./components/card.js"
+export { Checkbox } from "./components/checkbox.js"
+export {
+  DropdownMenu,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+} from "./components/dropdown-menu.js"
+export {
+  Field,
+  FieldLabel,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+  FieldContent,
+  FieldTitle,
+} from "./components/field.js"
+export {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupText,
+  InputGroupInput,
+  InputGroupTextarea,
+} from "./components/input-group.js"
+export { Input } from "./components/input.js"
+export {
+  Item,
+  ItemMedia,
+  ItemContent,
+  ItemActions,
+  ItemGroup,
+  ItemSeparator,
+  ItemTitle,
+  ItemDescription,
+  ItemHeader,
+  ItemFooter,
+} from "./components/item.js"
+export { Kbd, KbdGroup } from "./components/kbd.js"
 export { Label } from "./components/label.js"
 export { Separator } from "./components/separator.js"
+export { Switch } from "./components/switch.js"
+export { Textarea } from "./components/textarea.js"
 export { Badge, badgeVariants, type BadgeProps } from "./components/reui/badge.js"
 export { cn } from "./lib/utils.js"
